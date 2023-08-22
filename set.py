e677bc2ef8a7be8358143f6e38bc58e78a109ec1
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-08-22 19:25:57 +0530

    added and update element

--- set.py
+++ set.py
@@ -8,5 +8,16 @@ print(fruits)
 
 #Using set constructor
 
-fruits = set ({['apple','orange','banana']})
+fruits = set (['apple','orange','banana'])
+for item in fruits:
+    print(item)
+#print(fruits)
+#print(len(fruits))
+
+#add single element
+fruits.add('mango')
+print(fruits)
+
+#add multiple element
+fruits.update(['Red','Blue'])
 print(fruits)
