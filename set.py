11d32494fef79b66ea5448bf41e7ad6ac581a66c
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-08-22 20:56:07 +0530

    completed

--- set.py
+++ set.py
@@ -21,3 +21,25 @@ print(fruits)
 #add multiple element
 fruits.update(['Red','Blue'])
 print(fruits)
+
+fruits.remove('Red')
+fruits.discard('mango')
+print(fruits)
+fruits.pop()
+print(fruits)
+
+A = {1,2,3,4,5}
+B = {1,3,5,7,9}
+print(A|B)
+
+#Using Union method
+
+print(A.union(B))
+
+#set Differnce
+
+print(A-B)
+
+#Using differnce () method
+
+print(A.difference(B))
