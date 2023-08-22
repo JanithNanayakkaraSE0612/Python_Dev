dd0bbd1062d7df9a606cb60d5b3be0e40c2da76c
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-08-22 23:53:15 +0530

    Adding and Updating

---
+++ Dictonery.py
@@ -0,0 +1,18 @@
+my_first_dict = dict()
+
+#Using curly braces to initilialize a dictonary
+
+scores = {'Saman':75,'Nimal':80,'Kamal':65}
+
+print(scores['Saman'])
+print(scores.get('Nimal'))
+
+#Adding an element
+
+scores ['Sunil'] = 60
+print(scores)
+
+#Updating an element
+
+scores ['Saman'] =70
+print(scores)
