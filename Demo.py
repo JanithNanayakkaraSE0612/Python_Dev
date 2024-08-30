5027566e4ca341351a8f9de2246ef8060ac328b8
Janith-Nanayakkara <141998810+JanithNanayakkaraSE0612@users.noreply.github.com>
2024-08-30 12:57:35 +0530

    Test for and while case

---
+++ Demo.py
@@ -0,0 +1,18 @@
+# print("Hello")
+
+# Message = input()
+# print(Message)
+# print(type(Message))
+# num = 123
+# print(type(num))
+
+#for loop
+for i in range(5):
+    print("Hello " +str(i))
+
+    #while loop
+
+count = 3 
+while count > 0 :
+    print("Hello "+str(count) )
+    count-=1
