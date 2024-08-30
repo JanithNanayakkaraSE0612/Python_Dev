60f63cd804aa0db47f51dad86b62ed1bdae89a77
Janith-Nanayakkara <141998810+JanithNanayakkaraSE0612@users.noreply.github.com>
2024-08-30 14:12:08 +0530

    if and elif contiion test cases pass

--- Demo.py
+++ Demo.py
@@ -7,12 +7,32 @@
 # print(type(num))
 
 #for loop
-for i in range(5):
-    print("Hello " +str(i))
+# for i in range(5):
+#     print("Hello " +str(i))
 
     #while loop
 
-count = 3 
-while count > 0 :
-    print("Hello "+str(count) )
-    count-=1
+# count = 3 
+# while count > 0 :
+#     print("Hello "+str(count) )
+#     count-=1
+    
+# if else condition 
+
+# age = int(input("Say it your age : "))
+
+# if age >+ 18 :
+#     print("Succes login")
+# else :
+#     print("something error")
+    
+# else if condition 
+
+age = int(input("Enter you age : "))
+
+if age < 18 :
+    print("Yor Junior")
+elif age >= 75 : 
+    print("Your Senior")
+else:
+    print("out of coming wrong data")
