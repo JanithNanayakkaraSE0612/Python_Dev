63d7467d2d9d7d78f2663ccfe928d63e3c968900
Janith-Nanayakkara <141998810+JanithNanayakkaraSE0612@users.noreply.github.com>
2025-06-29 03:48:33 +0530

    Update Not in one all

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+x = {'1000':"Colombo", '2000':"Kandy", '3000':"Galle", '4000':"Matara", '5000':"Jaffna"}
+x['6000'] = "Trincomalee"  # Add a new key-value pair to the dictionary
+print(x)  # Print th    e dictionary
+print(x.keys())
+print(x.values())
+y=x['1000']  # Access the value associated with the key '1000'
+print(y)  # Print the value associated with the key '1000'
